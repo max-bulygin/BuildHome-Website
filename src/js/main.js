bbc4872381f9b504e880840b1ffaaf2eee3b3bc6
Max Bulygin <maksym.bulygin@gmail.com>
2016-09-02 21:47:35 +0300

    added main js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
-$("#accordion").accordion({
-    header: "h4",
+$('.main-menu__link').on('click', function () {
+    var $linkActive = 'main-menu__link--active';
+    $(this).addClass($linkActive)
+        .siblings('.main-menu__link--active').removeClass($linkActive);
+
+});
+$('#stickyMenu').sticky({topSpacing: 0,
+                        zIndex: 100,
+                        });
+var styles = {
+    backgroundColor: '#2a2d32',
+    opacity: 0.9
+};
+
+// $('#stickyMenu').on('sticky-start', function () {
+//     $('.header').css(styles);
+// });
+$('#slides').owlCarousel({
+    items: 3,
+    singleItem: true,
+    autoPlay: true
+});
+
+$('#accordion').accordion({
+    header: 'h4',
     animate: 200,
     heightStyle: 'content'
 });
